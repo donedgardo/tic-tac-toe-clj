52d46e9fa86f6074de16187fd43ee10110e9fbdf
Edgardo Carreras <edgardo.carreras@galaxydigital.io>
2021-09-23 04:47:46 -0400

    Refactor subscription

--- src/tic_tac_toe_web/board.cljs
+++ src/tic_tac_toe_web/board.cljs
@@ -57,28 +57,34 @@
       ((:reset online)))
     (reset-game)))
 
+
+(defn reset-game [game new-game]
+  (reset! game new-game))
+
+(defn subscribe-to-game [online game new-game]
+  (if (not (nil? online))
+    (subscribe-to-topic
+      (:node online)
+      (:room-id online)
+      (fn [msg]
+        (let [payload (js->clj (.parse js/JSON (. (. msg -data) (toString)) :keywordize-keys true))]
+          (js/console.log "payload" payload)
+          (cond
+            (= (payload "type") "reset")
+            (reset-game game new-game)
+            (= (payload "type") "play")
+            (swap! game play (payload "board-space"))
+            :else
+            nil))))))
+
 (defn tic-tac-toe-board [& [on-back options]]
   (let [{:keys [first-player ai-difficulty online]} options
-        new-game (create-game-factory {:first-player  first-player
-                                       :ai-difficulty ai-difficulty})
+        new-game (create-game-factory
+                   {:first-player  first-player
+                    :ai-difficulty ai-difficulty})
         game (atom new-game)
         handle-play #(on-play online game %)
-        reset-game #(reset! game new-game)
-        subscription (if (not (nil? online))
-                       (subscribe-to-topic
-                         (:node online)
-                         (:room-id online)
-                         (fn [msg]
-                           (let [payload (js->clj (.parse js/JSON (. (. msg -data) (toString)) :keywordize-keys true))]
-                             (js/console.log "payload" payload)
-                             (cond
-                               (= (payload "type") "reset")
-                               (reset-game)
-                               (= (payload "type") "play")
-                               (swap! game play (payload "board-space"))
-                               :else
-                               nil))))
-                       )]
+        subscription (subscribe-to-game online game new-game)]
     (fn []
       [:div.game
        (let [board (:board @game)
